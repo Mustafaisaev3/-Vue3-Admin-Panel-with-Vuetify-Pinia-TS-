2fecb097c6e4e7dc034a19dbcff66aab39a93546
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-11-12 01:06:04 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view/>
+      <HelloWorld></HelloWorld>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+</script>
