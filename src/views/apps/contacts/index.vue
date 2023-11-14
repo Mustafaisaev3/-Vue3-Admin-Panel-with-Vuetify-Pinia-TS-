89c8dc78845ab5ed64251c8d72dfc09908ee802d
Isaev Mustafa <mustafaisaev3@gmail.com>
2023-11-14 12:32:11 +0300

    Making Contact page

--- src/views/apps/contacts/index.vue
+++ src/views/apps/contacts/index.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
 import { ref } from 'vue';
-// common components
 import BaseBreadcrumb from '@/components/shared/BaseBreadcrumb.vue';
+import EditableTable from '@/components/table/EditableTable.vue';
 
-// theme breadcrumb
 const page = ref({ title: 'Contact List' });
 
 const breadcrumbs = ref([
@@ -18,5 +17,9 @@ const breadcrumbs = ref([
 
 <template>
     <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb>
-
+    <v-card elevation="10">
+        <v-card-text>
+            <EditableTable />
+        </v-card-text>
+    </v-card>
 </template>
